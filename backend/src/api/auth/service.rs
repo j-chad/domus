a1e8fe533eefc5b508be3a83b6f9ba65023c18c3
Jackson Chadfield <contact@jacksonc.dev>
2023-05-25 07:53:14 +1200

    add password hash function

--- backend/src/api/auth/service.rs
+++ backend/src/api/auth/service.rs
@@ -1,1 +1,13 @@
 
+use argon2::password_hash::rand_core::OsRng;
+use argon2::password_hash::SaltString;
+use argon2::{Argon2, PasswordHasher};
+
+pub fn hash_password(password: &str) -> Result<String> {
+    let password_bytes = password.as_bytes();
+    let salt = SaltString::generate(&mut OsRng);
+    let argon2 = Argon2::default();
+    let password_hash = argon2.hash_password(password_bytes, &salt)?;
+
+    Ok(password_hash.to_string())
+}
