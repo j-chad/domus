68ff6707e95553bb3086b901641c725e976211d5
Jackson Chadfield <contact@jacksonc.dev>
2023-08-08 21:44:58 +1200

    add routes

--- backend/src/handlers/auth.rs
+++ backend/src/handlers/auth.rs
@@ -1,10 +1,35 @@
-use axum::routing::get;
-use axum::Router;
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Router,
+};
 
 pub fn get_router() -> Router {
-    Router::new().route("/", get(handler))
+    Router::new()
+        .route("/register", post(register))
+        .route("/login", post(login))
+        .route("/logout", post(logout))
+        .route("/refresh_token", post(refresh_token))
+        .route("/user", get(get_user))
 }
 
-async fn handler() -> &'static str {
-    "Hello, World!"
+async fn register() -> impl IntoResponse {
+    StatusCode::NOT_IMPLEMENTED
+}
+
+async fn login() -> impl IntoResponse {
+    StatusCode::NOT_IMPLEMENTED
+}
+
+async fn logout() -> impl IntoResponse {
+    StatusCode::NOT_IMPLEMENTED
+}
+
+async fn refresh_token() -> impl IntoResponse {
+    StatusCode::NOT_IMPLEMENTED
+}
+
+async fn get_user() -> impl IntoResponse {
+    StatusCode::NOT_IMPLEMENTED
 }
