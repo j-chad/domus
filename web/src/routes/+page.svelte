5f7692e7ddfcd3d9e02ec025d77c4d45a463c616
Jackson Chadfield <contact@jacksonc.dev>
2023-09-12 08:49:50 +1200

    autorun prettier and eslint

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,10 +1,10 @@
+<script lang="ts">
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visita <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <div>
-		<a href="/blog">Blog</a>
+	<a href="/blog">Blog</a>
 	<a href="/about">About</a>
 </div>
-
-<script lang="ts">
-</script>
