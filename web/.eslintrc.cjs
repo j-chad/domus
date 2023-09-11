233ae7520586c9bc2d5e4017af2e8439ec3a8c5a
Jackson Chadfield <contact@jacksonc.dev>
2023-09-12 08:47:50 +1200

    fix some things

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -1,22 +1,5 @@
 module.exports = {
 	root: true,
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/strict-type-checked',
-		'plugin:@typescript-eslint/stylistic-type-checked',
-		'plugin:sonarjs/recommended',
-		'plugin:svelte/recommended',
-		'prettier'
-	],
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint', 'sonarjs'],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte'],
-		project: true,
-		tsconfigRootDir: __dirname,
-	},
 	env: {
 		browser: true,
 		es2017: true,
@@ -29,6 +12,29 @@ module.exports = {
 			parserOptions: {
 				parser: '@typescript-eslint/parser'
 			}
+		},
+		{
+			files: ['*.ts'],
+			parser: '@typescript-eslint/parser'
+		},
+		{
+			files: ['src/**/*'],
+			plugins: ['@typescript-eslint', 'sonarjs'],
+			parserOptions: {
+				sourceType: 'module',
+				ecmaVersion: 2020,
+				extraFileExtensions: ['.svelte'],
+				project: true,
+				tsconfigRootDir: __dirname
+			},
+			extends: [
+				'eslint:recommended',
+				'plugin:@typescript-eslint/strict-type-checked',
+				'plugin:@typescript-eslint/stylistic-type-checked',
+				'plugin:sonarjs/recommended',
+				'plugin:svelte/recommended',
+				'prettier'
+			]
 		}
 	]
 };
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visita <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div>
+		<a href="/blog">Blog</a>
+	<a href="/about">About</a>
+</div>
+
+<script lang="ts">
+</script>
