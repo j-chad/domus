d21fe27b8242c60b60d54dfe34ea2e4075639a12
Jackson Chadfield <contact@jacksonc.dev>
2023-09-23 18:48:02 +1200

    halve expiry time

--- backend/src/api/auth/utils.rs
+++ backend/src/api/auth/utils.rs
@@ -7,7 +7,10 @@ use pasetors::errors::Error as ClaimError;
 use pasetors::keys::{AsymmetricKeyPair, Generate};
 use pasetors::public;
 use pasetors::version4::V4;
+use std::time::Duration;
 use uuid::Uuid;
+
+const TOKEN_EXPIRY_TIME: Duration = Duration::new(30 * 60, 0); // 30 minutes
 
 pub fn verify_password(password: &str, hash: &str) -> Result<(), argon2::password_hash::Error> {
     let argon2 = Argon2::default();
@@ -27,13 +30,14 @@ pub fn hash_password(password: &str) -> Result<String, argon2::password_hash::Er
 }
 
 pub fn generate_auth_token(user: &User) -> Result<String, ClaimError> {
-    let mut claims = Claims::new()?;
+    let mut claims = Claims::new_expires_in(&TOKEN_EXPIRY_TIME)?;
     claims.issuer("domus-api.jacksonc.dev")?;
     claims.subject(user.id.as_hyphenated().to_string().as_str())?;
     claims.audience("domus.jacksonc.dev")?;
     claims.token_identifier(Uuid::new_v4().to_string().as_str())?;
 
     // Generate the keys and sign the claims.
+    // TODO: store the private key elsewhere and don't generate it every time.
     let kp = AsymmetricKeyPair::<V4>::generate()?;
     public::sign(&kp.secret, &claims, None, None)
 }
